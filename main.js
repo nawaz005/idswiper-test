fa8cf581e29e6e056aa573294e739d288b8d9018
nawaz <nawaz@bsb.in>
2014-08-31 10:57:16 +0530

    first commit in new branch

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    
+    var mySwiper = new Swiper('#swiper_container',{
+        loop:true,
+        grabCursor: true,
+        slidesPerSlide: 'auto',
+        paginationClickable: true
+    });
+};
